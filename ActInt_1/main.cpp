0fe6743ba324a1c007616030e9f87e1107036366
ATG-5222 <A01275222@tec.mx>
2022-09-01 12:57:01 -0500

    Ajustes menores

--- ActInt_1/main.cpp
+++ ActInt_1/main.cpp
@@ -18,48 +18,48 @@ void CodigosMaliciosos(string t1,string t2,string mc1,string mc2,string mc3) {
     //Archivo 1 - transmission1.txt
     cout << "transmission1.txt: " << endl;
     size_t pos = t1.find(mc1);
-    if(pos != string::npos) {
+    if(pos != string::npos){
         cout << "mcode1.txt - true, start at position " << pos << endl;
     }
-    else { 
+    else{ 
         cout << "mcode1.txt - false" << endl;
     }
     pos = t1.find(mc2);
-    if(pos != string::npos) {
+    if(pos != string::npos){
         cout << "mcode2.txt - true, start at position " << pos << endl;
     }
-    else {
+    else{
         cout << "mcode2.txt - false" << endl;
     }
     pos = t1.find(mc3);
-    if(pos != string::npos) {
+    if(pos != string::npos){
         cout << "mcode3.txt - true, start at position "<< pos << endl;
     }
-    else {
+    else{
         cout << "mcode3.txt - false" << endl;
     }
 
     //Archivo 2 - transmission2.txt
     cout << "\ntransmission2.txt: " << endl;
     pos = t2.find(mc1);
-    if(pos != string::npos) {
+    if(pos != string::npos){
         cout << "mcode1.txt - true, start at position " << pos << endl;
     }
-    else { 
+    else{ 
         cout << "mcode1.txt - false" << endl;
     }
     pos = t2.find(mc2);
-    if(pos != string::npos) {
+    if(pos != string::npos){
         cout << "mcode2.txt - true, start at position " << pos << endl;
     }
-    else {
+    else{
         cout << "mcode2.txt - false" << endl;
     }
     pos = t2.find(mc3);
-    if(pos != string::npos) {
+    if(pos != string::npos){
         cout << "mcode3.txt - true, start at position "<< pos << endl;
     }
-    else {
+    else{
         cout << "mcode3.txt - false" << endl;
     }
 }
@@ -70,19 +70,19 @@ int main() {
     string l1,l2,l3,l4,l5;
     ifstream txt1("transmission1.txt"), txt2("transmission2.txt"), txt3("mcode1.txt"), txt4("mcode2.txt"), txt5("mcode3.txt");
     //Lectura de archivos txt y almacenamiento de información
-    while(getline(txt1, l1)) {
+    while(getline(txt1, l1)){
         t1 = t1 + l1;
     }
-    while(getline(txt2, l2)) {
+    while(getline(txt2, l2)){
         t2 = t2 + l2;
     }
-    while(getline(txt3, l3)) {
+    while(getline(txt3, l3)){
         mc1 = mc1 + l3;
     }
-    while(getline(txt4, l4)) {
+    while(getline(txt4, l4)){
         mc2 = mc2 + l4;
     }
-    while(getline(txt5, l5)) {
+    while(getline(txt5, l5)){
         mc3 = mc3 + l5;
     }
 
