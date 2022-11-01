4530ec2c2fafa39acc75c95fbc1f503c09ec2843
ATG-5222 <A01275222@tec.mx>
2022-10-31 22:54:23 -0600

    Añadir comentarios e includes iniciales

---
+++ GraphColoring/main.cpp
@@ -0,0 +1,11 @@
+// =========================================================
+// File: main.cpp
+// Authors:
+// Aldo Tena García - A01275222
+// Renato Sebastían Ramirez Calva - A01275401
+// Date: 07-09-2022
+// =========================================================
+
+#include <bits/stdc++.h>
+
+using namespace std;
