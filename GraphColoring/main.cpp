0f4639ab13549e49ed99f8fe8fd3c38023c42dbd
ATG-5222 <A01275222@tec.mx>
2022-10-31 22:58:53 -0600

    Recibir entrada de datos del grafo

--- GraphColoring/main.cpp
+++ GraphColoring/main.cpp
@@ -7,5 +7,35 @@
 // =========================================================
 
 #include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 
 using namespace std;
+
+int main(){
+
+    //Declaracion de variables iniciales
+    int size=0;
+    cin>>size;
+    vector<vector<bool>> grafo;
+    int n = 50; 
+    int colors[size];
+
+    // Se llena el vector dependiendo del tamano del grafo
+    for (int i = 0; i < size; i++) {
+        vector<bool> temp;
+        for (int j = 0; j < size; j++) {
+            bool tempBool;
+            cin>>tempBool;
+            temp.push_back(tempBool);
+        }
+        grafo.push_back(temp);
+    }
+    
+    // Se establece el valor de los colores dependiendo del tamano
+    for (int i = 0; i < size; i++){
+        colors[i] = 0;
+    }
+
+    return 0;
+}
