d59efa30f876097d100cdc7b0a4183ef25d98489
ATG-5222 <A01275222@tec.mx>
2022-11-07 12:10:28 -0600

    Traspaso de Repl.it

--- Problem10881/main.cpp
+++ Problem10881/main.cpp
@@ -1,4 +1,36 @@
-// Internal Rate of Return 
+// Internal Rate of return
 // Aldo Tena García - A01275222
 // Renato Sebastían Ramirez Calva - A01275401
-// Marco Camalich
+// Marco Antonio Camalich Pérez - A01351725
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int cashFlow[100];
+int main(){
+    int test;
+    while(1){
+        scanf("%d", &test);
+        if(test==0){
+            break;
+        }
+        for(int i=0; i<=test; i++){
+            scanf("%d", &cashFlow[i]);
+        }
+        double result,low=-1,high=10000;
+        for(int j=1;j<=107;j++){
+            result=(low+high)/2;
+            double sum=0;
+            for(int k=0;k<=test;k++){
+                sum += (double(cashFlow[k]))/pow(1.0+result,k);
+            }
+            if(sum>0){
+                low=result;
+            }
+            else{
+                high=result;
+            }
+        }
+    printf("%0.2lf\n",result);
+    }
+}
