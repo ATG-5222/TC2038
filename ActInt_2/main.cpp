552cf68b973233dff63d8372570cb13508829d4a
ATG-5222 <A01275222@tec.mx>
2022-11-11 00:05:41 -0600

    Ajustes en autores

--- ActInt_2/main.cpp
+++ ActInt_2/main.cpp
@@ -3,5 +3,5 @@
 // Authors:
 // Aldo Tena García - A01275222
 // Renato Sebastían Ramirez Calva - A01275401
-// Date: 07-09-2022
+// Date: 11/11/2022
 // =========================================================
--- ActInt_2/tsp.h
+++ ActInt_2/tsp.h
@@ -1,3 +1,11 @@
+// =========================================================
+// File: main.cpp
+// Authors:
+// Aldo Tena García - A01275222
+// Renato Sebastían Ramirez Calva - A01275401
+// Date: 11/11/2022
+// =========================================================
+
 #include<bits/stdc++.h>
 #include <iostream>
 #include <vector>
